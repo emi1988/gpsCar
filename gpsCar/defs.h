689b3bd0204eb8966204b697d9b0ced6b3f0892e
emi1988 <ele14@ich-habe-fertig.com>
2016-02-25 18:15:35 +0000

    definition file defs.h added

---
+++ gpsCar/defs.h
@@ -0,0 +1,33 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <QString>
+#include <QtSerialPort/QSerialPort>
+
+typedef struct stSerialPortSettings
+{
+    QString portName;
+    qint32 baudRate;
+    QSerialPort::DataBits dataBits;
+    QSerialPort::Parity parity;
+    QSerialPort::StopBits stopBits;
+    QSerialPort::FlowControl flowControl;
+};
+
+typedef struct stGPSdata
+{
+    QString timeStamp;
+    QString longitude; //längengrad
+    QString longitudeAlignment; //E(ast) or W(est)
+    QString latitude; //breitengrad
+    QString latitudeAlignment; // N(orth) or S(outh)
+    QString satelliteAmount;
+    QString horizontalPrecision;
+    QString altitude;
+
+    QString longitudeDecimal;
+    QString latitudeDecimal;
+};
+
+
+#endif // DEFS_H
